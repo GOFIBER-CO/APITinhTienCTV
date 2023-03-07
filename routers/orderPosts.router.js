0d6e5d55f85c06041587ae5a58bf325762bfbc32
zukatahara <quyenpro7a@gmail.com>
2023-03-07 14:43:01 +0700

    new order

--- routers/orderPosts.router.js
+++ routers/orderPosts.router.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const orderPostController = require("./../controllers/oderPosts.controller");
+const authorize = require("../middleware/authorize");
 
-router.post("/order-post/insert", orderPostController.insertNewOrderPosts);
-router.get("/order-post/list", orderPostController.getListOrderPosts);
-router.delete("/order-post/delete/:id", orderPostController.deleteRecord);
-router.patch("/order-post/update/:id", orderPostController.updateRecord);
+router.post(
+  "/order-post/insert",
+  authorize(),
+  orderPostController.insertNewOrderPosts
+);
+router.get(
+  "/order-post/list",
+  authorize(),
+  orderPostController.getListOrderPosts
+);
+router.delete(
+  "/order-post/delete/:id",
+  authorize(),
+  orderPostController.deleteRecord
+);
+router.patch(
+  "/order-post/update",
+  authorize(),
+  orderPostController.updateRecord
+);
 module.exports = router;
